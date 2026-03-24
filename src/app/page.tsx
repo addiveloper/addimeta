15076e3927446dd0b4e64820a8fd466f2c87f13d
addiveloper <addi.veloper@gmail.com>
2026-03-23 20:03:04 -0400

    ✨ add main section to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
 export default function Home() {
   return (
-    <main>
-      <h1>Home</h1>
+    <main className="flex h-dvh items-center justify-center">
+      <section className="flex flex-col gap-4">
+        <h1 className="text-6xl font-bold text-primary font-heading text-center">
+          addimeta
+        </h1>
+        <p className="text-muted-foreground text-center">
+          Ve los metadatos de tus imágenes. Gratis, rápido y sin registrarte.
+        </p>
+      </section>
     </main>
   );
 }
